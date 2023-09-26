b3055273f6493afdaaeab0bf12f6017d7c14427a
Rom-Freedom <romeo.dark@mail.ru>
2023-09-26 21:33:31 +0300

    "to form header"

--- index.php
+++ index.php
@@ -9,10 +9,25 @@
 <body>
     <div class="container">
         <div class="left">
+            <br>
+            <div class="message"></div>
+            <br>
 
+            <div class="header">
+                <h2 class="animation a1">Добро пожаловать!</h2>
+                <h4 class="animation a2">Напишите Ваше письмо</h4>
+            </div>
+
+            <div class="form">
+                <input type="text" class="form-field animation a1" id="user_name" placeholder="Введите Ваше имя:">
+                <input type="text" class="form-field animation a2" id="user_email" placeholder="Введите Ваш e-mail:">
+                <input type="text" class="form-field animation a3" id="user_phone" placeholder="+79814125678">
+                <textarea id="text_comment" class="form-field animation a4" type="text" placeholder="Напишите Ваше письмо"></textarea>
+                <button class="animation a5" id="btn_submit">Отправить</button>
+            </div>
         </div>
         <div class="right">
-            git  s
+            
         </div>
     </div>
 </body>
