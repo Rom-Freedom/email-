1e20fddc0d2bf822690862516d37fbcbaa4600a4
Rom-Freedom <romeo.dark@mail.ru>
2023-09-28 15:27:35 +0300

    "to correct errors"

--- index.php
+++ index.php
@@ -5,59 +5,59 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sennding letters by email</title>
     <link rel="stylesheet" href="style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js" type="javascript"></script>
-<script>
-    $(document).ready(function(){  
-        $('#btn_submit').click(function(){   // to commit a click on a buton
-            var user_name = $('#user_name').val(); //to get value user
-            var user_email = $('#user_email').val();
-            var user_phone = $('#user_phone').val();
-            var text_comment = $('#text_comment').val();
-            var re_em = /^[\w-\.]+@[\w-]+\.[a+z]{2,4}$/i; //regular expression for email
-            var ru_nu = /^[\d\+][\d\(\)\ -]{10,14}\d$/;  //regular expression for phone
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js" type="text/javascript"></script>
+    <script>
+        $(document).ready(function(){  
+            $('#btn_submit').click(function(){   // to commit a click on a buton
+                var user_name = $('#user_name').val(); //to get value user
+                var user_email = $('#user_email').val();
+                var user_phone = $('#user_phone').val();
+                var text_comment = $('#text_comment').val();
+                var re_em = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i; //regular expression for email
+                var ru_nu = /^[\d\+][\d\(\)\ -]{10,14}\d$/;  //regular expression for phone
 
-            if(user_name == ''){
-                document.getElementById('user_name').style.borderColor = 'red';
-            }else{
-                document.getElementById('user_name').style.borderColor = 'green';
-            }
-
-            if(user_email == '' || !re_em.test(user_email)){
-                document.getElementById('user_email').style.borderColor = 'red';
-            }else{
-                document.getElementById('user_email').style.borderColor = 'green';
-            }
-
-            if(user_phone == ''|| !re_nu(user-phone)){
-                document.getElementById('user_phone').style.borderColor = 'red';
-            }else{
-                document.getElementById('user_phone').style.borderColor = 'green';
-            }
-
-            if(text-comment == ''){
-                document.getElementById('text-comment').style.borderColor = 'red';
-            }else{
-                document.getElementById('text-comment').style.borderColor = 'green';
-            }
-
-            $.ajax({
-                url: "action_php",
-                type: "post",
-                dataType: "json",
-                data: {
-                    "user_name": user_name,
-                    "user_email": user_email,
-                    "user-phone": user_phone,
-                    "text_comment": text_comment
-                },
-                // to get responce from a server
-                success: function(data){
-                    $('.messages').html(data.reset);
+                if(user_name == ''){
+                    document.getElementById('user_name').style.borderColor = 'red';
+                }else{
+                    document.getElementById('user_name').style.borderColor = 'green';
                 }
-            })
+
+                if(user_email == '' || !re_em.test(user_email)){
+                    document.getElementById('user_email').style.borderColor = 'red';
+                }else{
+                    document.getElementById('user_email').style.borderColor = 'green';
+                }
+
+                if(user_phone == ''|| !re_nu.test(user_phone)){
+                    document.getElementById('user_phone').style.borderColor = 'red';
+                }else{
+                    document.getElementById('user_phone').style.borderColor = 'green';
+                }
+
+                if(text_comment == ''){
+                    document.getElementById('text_comment').style.borderColor = 'red';
+                }else{
+                    document.getElementById('text_comment').style.borderColor = 'green';
+                }
+
+                $.ajax({
+                    url: "action_php",
+                    type: "post",
+                    dataType: "json",
+                    data: {
+                        "user_name": user_name,
+                        "user_email": user_email,
+                        "user_phone": user_phone,
+                        "text_comment": text_comment
+                    },
+                    // to get responce from a server
+                    success: function(data){
+                        $('.messages').html(data.reset);
+                    }
+                })
+            });
         });
-    });
-</script>
+    </script>
 </head>
 
 
@@ -81,9 +81,7 @@
                 <button class="animation a5" id="btn_submit">Отправить</button>
             </div>
         </div>
-        <div class="right">
-            
-        </div>
+        <div class="right"></div>
     </div>
 </body>
 </html>
